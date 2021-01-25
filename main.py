bf0a685d563ead74250c84aa760db338b48ccd78
edmundwsy <edmundwsy@outlook.com>
2021-01-25 15:04:54 +0800

    fix bugs according to origin repo

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main():
     options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=options)
     driver.get("http://jkrb.xjtu.edu.cn/EIP/user/index.htm")
-    wait = WebDriverWait(driver=driver, timeout=10)
+    wait = WebDriverWait(driver=driver, timeout=30)
     wait.until((EC.url_contains("org.xjtu.edu.cn")))
     elem = wait.until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="form1"]/input[1]'))
@@ -61,10 +61,7 @@ def main():
         driver.switch_to.frame(iframe)
         iframe = driver.find_element_by_xpath("//iframe[@onload='__iframe_onload1()']")
         driver.switch_to.frame(iframe)
-        # driver.find_element_by_xpath("//input[@value='绿色']").click()
-        # logger.info("Check green status")
-        # driver.find_element_by_xpath("//input[@id='mini-4$ck$0']").click()
-        # logger.info("Check has Xi'an QR")
+
         temp = str(round(36 + random(), 1))
         driver.find_element_by_xpath(
             "//input[@placeholder='请准确填写体温，格式如:36.5']"
@@ -77,8 +74,20 @@ def main():
         driver.switch_to.frame(iframe)
         submit_btn = driver.find_element_by_xpath("//a[@id='sendBtn']")
         submit_btn.click()
-        driver.find_element_by_xpath("//*[@id='mini-17']").click()
-        logger.info("Successful submit!")
+        elem = driver.find_element_by_xpath("//*[@id='mini-17']")
+        elem.click()
+        try:
+            driver.switch_to.default_content()
+            driver.implicitly_wait(1)
+            iframe = driver.find_element_by_xpath(
+                "//iframe[@onload='__iframe_onload4()']"
+            )
+            driver.switch_to.frame(iframe)
+            elem = driver.find_element_by_xpath("//*[@id='mini-19$content']")
+            logger.info(elem.text)
+        except NoSuchElementException:
+            logger.info("Successful submit!")
+
     except NoSuchElementException:
         driver.switch_to.default_content()
         iframe = driver.find_element_by_xpath("//iframe[@onload='__iframe_onload5()']")
